215fcf84376948739dd57bcbb613a13308bf5ee7
xuyundong <xuyundong@douban.com>
2022-06-11 01:07:15 +0800

    add demo

--- demo/api.py
+++ demo/api.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/api")
 def hello_world():
     return "<p>Hello, API Service!</p>"
+
+
+@app.route("/")
+def index():
+    api_header = request.headers.get("API-VERSIOn")
+    return "Api Service Index: %s" % api_header
 
 
 def main():
